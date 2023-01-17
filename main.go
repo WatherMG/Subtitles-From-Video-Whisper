87a78fe8a415a7a8fc0901d564c860ee11c84fa3
Alex Sysoev <dr.wather@gmail.com>
2023-01-17 23:58:57 +0300

    remove redundant code, optimize, add gorutine

--- main.go
+++ main.go
@@ -2,117 +2,107 @@ package main
 
 import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"io/fs"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 const AudioExtension = ".mp3"
 const VideoExtension = ".mp4"
-const VideoRoot = "D:\\Projects\\whisper\\video"
-const AudioRoot = "D:\\Projects\\whisper\\audio"
+const SubtitleExtension = ".srt"
+const VideoDir = "D:\\Projects\\whisper\\video"
+const AudioDir = "D:\\Projects\\whisper\\audio"
+const OutputDir = "D:\\Projects\\whisper\\output"
 
-func getFilesPath(root, pattern string) ([]string, error) {
-	var matches []string
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+var wg sync.WaitGroup
+
+var maxFilesPerDecode = 5
+var maxFilesPerTranscribe = 1
+var maxFilesPerPass int
+
+func isOutputFileExist(outputFile string, extension string) bool {
+	outputFile = strings.Replace(outputFile, filepath.Ext(outputFile), extension, -1)
+	if _, err := os.Stat(outputFile); os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+func createFiles(inputDir string, isCreatingMP3 bool) {
+	var extension string
+	if isCreatingMP3 {
+		extension = VideoExtension
+		maxFilesPerPass = maxFilesPerDecode
+	} else {
+		extension = AudioExtension
+		maxFilesPerPass = maxFilesPerTranscribe
+	}
+	sem := make(chan struct{}, maxFilesPerPass)
+	defer close(sem)
+	_ = filepath.Walk(inputDir, func(inputFile string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if info.IsDir() {
+		if info.IsDir() || filepath.Ext(inputFile) != extension {
 			return nil
 		}
-		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
-			return err
-		} else if matched {
-			matches = append(matches, path)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return matches, nil
-}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			relativePath, _ := filepath.Rel(inputDir, inputFile)
+			outputFile := filepath.Join(OutputDir, relativePath)
+			outputDir := filepath.Dir(outputFile)
 
-func setFileExtension(parent string, filePath string, old string, new string) string {
-	return parent + "\\" + filepath.Base(strings.Replace(filePath, old, new, 1))
-}
-
-func getOutputDir(file string, from string, to string) string {
-	return strings.Replace(filepath.Dir(file), from, to, 1)
-}
-
-func makeFile(path string, subtitle bool) {
-	var existFiles = 0
-	var createdFiles = 0
-	var outputDir string
-	var typeOfFile = "*" + VideoExtension
-	if subtitle {
-		typeOfFile = "*" + AudioExtension
-	}
-	files, err := getFilesPath(path, typeOfFile)
-	if err == nil {
-		var existFile error
-		for _, file := range files {
-			if subtitle {
-				outputDir = getOutputDir(file, "audio", "subtitles")
-				_, existFile = os.Stat(setFileExtension(outputDir, file, AudioExtension, AudioExtension+".srt"))
-			} else {
-				outputDir = getOutputDir(file, "video", "audio")
-				_, existFile = os.Stat(setFileExtension(outputDir, file, VideoExtension, AudioExtension))
-			}
-			aErr := os.Mkdir(outputDir, 0777)
-			if aErr != nil && !os.IsExist(aErr) {
-				log.Fatal(aErr)
-			}
-			if os.IsNotExist(existFile) {
-				if subtitle {
-					var back = "cd ..\\; "
-					var venv = ".\\activate.ps1; " + back + back
-					var language = "--language en "
-					var model = "--model tiny.en "
-					var device = "--device cuda "
-					var out = `--output_dir "` + outputDir + `"`
-					var whisper = "whisper " + language + model + device + out + ` -- "` + file + `"`
-					cmd := exec.Command("powershell", venv+whisper)
-					cmd.Dir = "D:\\Projects\\whisper\\.venv\\Scripts"
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
-					log.Printf("Start transcribe %s ", filepath.Base(file))
-					err := cmd.Run()
+			if isOutputFileExist(outputFile, AudioExtension) || !isCreatingMP3 {
+				_ = os.MkdirAll(outputDir, os.ModePerm)
+				if isCreatingMP3 {
+					sem <- struct{}{}
+					start := time.Now()
+					log.Printf("Start decode %s", filepath.Base(inputFile))
+					err = ffmpeg.Input(inputFile).
+						Output(outputFile, ffmpeg.KwArgs{"b:a": "320K", "vn": ""}).OverWriteOutput().Run()
 					if err != nil {
-						log.Printf("Something went wrong: %s", err)
+						log.Printf("Something went wrong in decode: %s", err)
 					}
-					log.Printf("File %s is transcribed", filepath.Base(file))
+					log.Printf("Finish decode %s in %s", filepath.Base(inputFile), time.Since(start))
+					<-sem
 				} else {
-					err := ffmpeg.Input(file).
-						Output(setFileExtension(outputDir, file, VideoExtension, AudioExtension), ffmpeg.KwArgs{"b:a": "320K", "vn": ""}).
-						OverWriteOutput().Run()
-					createdFiles += 1
-					if err != nil {
-						println(err)
+					if isOutputFileExist(outputFile, ".mp3"+SubtitleExtension) {
+						sem <- struct{}{}
+						var activateVenv = ".\\activate.ps1; "
+						cmd := exec.Command("powershell", activateVenv+"whisper --language en "+
+							"--model medium.en --device cuda --o "+strconv.Quote(outputDir)+" --verbose False -- "+strconv.Quote(inputFile))
+						cmd.Dir = "D:\\Projects\\whisper\\.venv\\Scripts"
+						cmd.Stdout = os.Stdout
+						cmd.Stderr = os.Stderr
+						start := time.Now()
+						log.Printf("Start transcribe %s", filepath.Base(inputFile))
+						err := cmd.Run()
+						if err != nil {
+							log.Printf("Something went wrong in transcribe: %s", err)
+						}
+						log.Printf("Finish transcribe %s in %s", filepath.Base(inputFile), time.Since(start))
+						<-sem
 					}
 				}
-			} else {
-				existFiles += 1
 			}
-		}
-	}
-	println("Created audio:", createdFiles)
-	println("Skipped video:", existFiles)
+		}()
+
+		return nil
+	})
+	wg.Wait()
 }
 
 func main() {
-
-	start := time.Now()
-
-	makeFile(VideoRoot, false)
-	makeFile(AudioRoot, true)
-
-	elapsed := time.Since(start)
-	log.Printf("Time to complete %s", elapsed)
-
+	startTime := time.Now()
+	createFiles(VideoDir, true)
+	createFiles(AudioDir, false)
+	elapsed := time.Since(startTime)
+	log.Printf("Time to complete: %s\n", elapsed)
 }
